e216e513d8fbcc4ce366947237394cf2eb2d0688
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2023-09-17 03:47:07 +0600

    Author Details Added

--- src/Blog/Blog.jsx
+++ src/Blog/Blog.jsx
@@ -1,8 +1,38 @@
 import PropTypes from "prop-types";
 
 export default function Blog({ blog }) {
-  console.log(blog);
-  return <div></div>;
+  const {
+    title,
+    cover,
+    author,
+    author_img,
+    reading_time,
+    posted_date,
+    hashtags,
+  } = blog;
+  return (
+    <div>
+      <img src={cover} alt={`Cover picture of the title`} />
+      <div className="flex justify-between ">
+        <div className="flex gap-3">
+          <img className="w-14" src={author_img} alt="" />
+          <div>
+            <h3 className="text-2xl">{author}</h3>
+            <p>{posted_date}</p>
+          </div>
+        </div>
+        <div>
+          <span>{reading_time} min read</span>
+        </div>
+      </div>
+      <h2 className="text-4xl">{title}</h2>
+      <p>
+        {hashtags.map((hash, idx) => (
+          <span key={idx}>#{hash}</span>
+        ))}
+      </p>
+    </div>
+  );
 }
 
 Blog.propTypes = {
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import Blog from "../../Blog/Blog";
 
 export default function Blogs() {
   const [blogs, setBlogs] = useState([]);
@@ -13,9 +14,9 @@ export default function Blogs() {
   return (
     <div className="w-2/3">
       <h1 className="text-4xl">Blogs: {blogs.length}</h1>
-      {
-        
-      }
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
     </div>
   );
 }
